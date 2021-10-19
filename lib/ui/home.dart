b9a19f510fad5a4a70362181f59b76127728f54b
FlutLab.io <>
2021-10-19 16:47:19 +0000

    Autosave

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -87,7 +87,7 @@ class _HomeState extends State<Home> {
         ],
       );
   Widget get _listView => ListView.builder(
-        itemCount: 6,
+        itemCount: userData.lenght,
         itemBuilder: (context, index) {
           return _listViewCard;
         },
