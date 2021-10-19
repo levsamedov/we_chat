c557f60ee9c99dabf8ad4373201cad25a54d3b52
FlutLab.io <>
2021-10-19 17:51:14 +0000

    Autosave

--- lib/components/list_view_card.dart
+++ lib/components/list_view_card.dart
@@ -14,7 +14,7 @@ class ListViewCard extends StatelessWidget {
       ),
       title: Wrap(
         children: [
-          Text(user!.name),
+          Text(user. name!),
         ],
       ),
     );
